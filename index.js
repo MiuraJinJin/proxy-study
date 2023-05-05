85ea876cb257dd01392baa33b13ccf43b682f6ac
miura.masaoki <jinjin@miuramasaokiMacBook-Air.local>
2023-05-05 19:53:12 +0900

    クエリパラメーター修正

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 const url = require("url");
+const querystring = require('querystring');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -17,9 +18,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/city-weather-data", limiter, (req, res) => {
-  const params = url.parse(req.url).query;
+  const params = querystring.parse(url.parse(req.url).query);
+  params["key"] = process.env.API_KEY_CITY_WEATHER;
+  params["api"] = "no";
+
   createProxyMiddleware({
-    target: process.env.API_URL_CITY_WEATHER + "?" + params,
+    target: process.env.API_URL_CITY_WEATHER + "?" + querystring.stringify(params),
     changeOrigin: true,
     pathRewrite: {
       [`^/city-weather-data`]: ""
